f73178381ce6c9dfff44eed5ab2901bd65c0f5b2
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2022-05-11 21:24:05 +0200

    Finished first version of main settings screen

--- app/src/main/java/com/example/simpletasks/SettingsActivity.java
+++ app/src/main/java/com/example/simpletasks/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.simpletasks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,4 +20,28 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    public void onAddPinClicked(View view) {
+        Intent intent = new Intent(this, AddPinActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void onOpenDisplaySettingsClicked(View ignored) {
+        Intent displaySettingsIntent = new Intent(Settings.ACTION_DISPLAY_SETTINGS);
+        startActivity(displaySettingsIntent);
+    }
+
+    public void onRemovePinClicked(View view) {
+        Intent intent = new Intent(this, RemovePinActivity.class);
+        startActivity(intent);
+    }
+
+    public void onBackClicked(View view) {
+        super.onBackPressed();
+    }
+
+
+    public void onLogoutClicked(View view) {
+        // TODO: implement when user info is ready
+    }
 }
