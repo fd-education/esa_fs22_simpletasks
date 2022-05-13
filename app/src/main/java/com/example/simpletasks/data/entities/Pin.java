3f96dd12c6062879ee8243989c0d1f2d8ebc5cbf
nicola.buehler@students.ffhs.ch <nicola.buehler@students.ffhs.ch>
2022-05-13 15:44:17 +0200

    Merged Pin classes and introduced hash field

--- app/src/main/java/com/example/simpletasks/data/entities/Pin.java
+++ app/src/main/java/com/example/simpletasks/data/entities/Pin.java
@@ -1,7 +1,12 @@
 package com.example.simpletasks.data.entities;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Random;
 
 @Entity(tableName = "pins")
 public class Pin {
@@ -9,10 +14,38 @@ public class Pin {
     @PrimaryKey(autoGenerate = true)
     private int id;
 
-    private int pin;
+    private int hash;
 
-    public Pin(int pin){
+    @Ignore
+    private String pin;
+
+    @Ignore
+    public Pin(final String pin) {
+        validatePin(pin);
         this.pin = pin;
+        this.hash = pin.hashCode();
+    }
+
+    private void validatePin(final String pin) {
+        Objects.requireNonNull(pin, "PIN must not be null");
+        if (!pin.matches("^\\d{5,}$")){
+            throw new IllegalArgumentException(pin+" is not a valid PIN; must consist of at least 5 digits");
+        }
+    }
+
+    public Pin(int hash) {
+        this.hash = hash;
+    }
+
+    public static Pin fromInt(int pin) {
+        String pinStr = String.format(Locale.getDefault(), "%05d", pin);
+        return new Pin(pinStr);
+    }
+
+    static public Pin random() {
+        Random random = new Random();
+        int randomInt = random.nextInt(10_000);
+        return Pin.fromInt(randomInt);
     }
 
     public int getId() {
@@ -23,11 +56,17 @@ public class Pin {
         this.id = id;
     }
 
-    public int getPin() {
+    public String getPin() {
         return pin;
     }
 
-    public void setPin(int pin) {
+    public void setPin(String pin) {
+        validatePin(pin);
         this.pin = pin;
+        this.hash = pin.hashCode();
+    }
+
+    public int getHash() {
+        return hash;
     }
 }
