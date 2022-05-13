e8d3caee05b8aa510689bb938f54d2d325cb0bc0
jan.niklaus@students.ffhs.ch <jan.niklaus@students.ffhs.ch>
2022-05-13 15:45:29 +0200

    improved task step list ui

--- app/src/main/java/com/example/simpletasks/adapters/EditTaskStepsListAdapter.java
+++ app/src/main/java/com/example/simpletasks/adapters/EditTaskStepsListAdapter.java
@@ -23,6 +23,7 @@ public class EditTaskStepsListAdapter extends RecyclerView.Adapter<EditTaskSteps
      * setting them in the adapter
      */
     class TaskListViewHolder extends RecyclerView.ViewHolder {
+        private final ImageButton dragTaskStepButton;
         private final TextView titleTaskStep;
         private final TextView taskStepType;
         private final ImageView taskImage;
@@ -32,6 +33,7 @@ public class EditTaskStepsListAdapter extends RecyclerView.Adapter<EditTaskSteps
 
         private TaskListViewHolder(View itemView) {
             super(itemView);
+            dragTaskStepButton = itemView.findViewById(R.id.dragTaskStepButton_editTask);
             titleTaskStep = itemView.findViewById(R.id.taskStepTitle_editTask);
             taskStepType = itemView.findViewById(R.id.taskStepType_editTask);
             taskImage = itemView.findViewById(R.id.taskStepImage_editTask);
@@ -61,6 +63,9 @@ public class EditTaskStepsListAdapter extends RecyclerView.Adapter<EditTaskSteps
     public void onBindViewHolder(@NonNull TaskListViewHolder holder, int position) {
         if (taskSteps != null) {
             TaskStep currentTaskStep = taskSteps.get(position);
+            holder.dragTaskStepButton.setOnClickListener(v -> {
+                //TODO
+            });
             holder.titleTaskStep.setText(currentTaskStep.getTitle());
             holder.taskStepType.setText(context.getString(R.string.type, currentTaskStep.getType()));
             holder.taskImage.setImageResource(R.drawable.ic_launcher_background/*TODO change */);
